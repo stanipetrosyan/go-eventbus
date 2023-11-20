6588f87c1980fe48c6ae61ef0f85484b2d33ec4e
StaniPetrosyan <stani.petrosyan@gmail.com>
2023-11-20 14:41:57 +0100

    improved tests

--- channel_test.go
+++ channel_test.go
@@ -11,7 +11,7 @@ func TestSubscriberHandler(t *testing.T) {
 	var eventBus = NewEventBus()
 	var wg sync.WaitGroup
 
-	t.Run("should handle a message using channels", func(t *testing.T) {
+	t.Run("should handle a message", func(t *testing.T) {
 		wg.Add(1)
 
 		eventBus.Channel("my-channel").Subscriber().Listen(func(context Context) {
@@ -24,7 +24,7 @@ func TestSubscriberHandler(t *testing.T) {
 		wg.Wait()
 	})
 
-	t.Run("should handle message for more of one handlers", func(t *testing.T) {
+	t.Run("should handle message with many subscribers", func(t *testing.T) {
 		wg.Add(2)
 
 		eventBus.Channel("newaddress").Subscriber().Listen(func(context Context) {
@@ -47,7 +47,7 @@ func TestProcessorHandler(t *testing.T) {
 	var eventBus = NewEventBus()
 	var wg sync.WaitGroup
 
-	t.Run("should pass processor handler", func(t *testing.T) {
+	t.Run("should pass processor", func(t *testing.T) {
 		wg.Add(2)
 
 		eventBus.Channel("my-channel").Subscriber().Listen(func(context Context) {
@@ -58,24 +58,6 @@ func TestProcessorHandler(t *testing.T) {
 		eventBus.Channel("my-channel").Processor(func(message Message) bool {
 			wg.Done()
 			return message.Data == "Hi There"
-		})
-
-		message := CreateMessage().SetBody("Hi There")
-		eventBus.Channel("my-channel").Publisher().Publish(message)
-		wg.Wait()
-	})
-
-	t.Run("should pass message to handler created afted interceptor", func(t *testing.T) {
-		wg.Add(2)
-
-		eventBus.Channel("my-channel").Processor(func(message Message) bool {
-			wg.Done()
-			return message.Data == "Hi There"
-		})
-
-		eventBus.Channel("my-channel").Subscriber().Listen(func(context Context) {
-			assert.Equal(t, "Hi There", context.Result().Data)
-			wg.Done()
 		})
 
 		message := CreateMessage().SetBody("Hi There")
