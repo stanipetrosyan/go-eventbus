453cfec8cda822d89a726bb2e7c3a8aff100794c
stanipetrosyan <stani.petrosyan@gmail.com>
2024-08-23 13:18:50 +0200

    refactor: used sync Map in eventbus to manage safately channel

--- eventbus.go
+++ eventbus.go
@@ -1,29 +1,29 @@
 package goeventbus
 
-import "log/slog"
+import (
+	"log/slog"
+	"sync"
+)
 
 type EventBus interface {
 	Channel(adress string) Channel
 }
 
 type defaultEventBus struct {
-	channels map[string]Channel
+	channels sync.Map
 }
 
 // Returns a Channel with that address. If it doesn't exist, it returns a new one
 func (e *defaultEventBus) Channel(address string) Channel {
-	_, exists := e.channels[address]
-	if !exists {
-		e.channels[address] = newChannel(address)
-	}
+	channel, _ := e.channels.LoadOrStore(address, newChannel(address))
 
 	slog.Info("Channel created", slog.String("name", address))
-	return e.channels[address]
+	return channel.(Channel)
 }
 
 // Create a new eventbus with default parameters
 func NewEventBus() EventBus {
 	return &defaultEventBus{
-		channels: map[string]Channel{},
+		channels: sync.Map{},
 	}
 }
